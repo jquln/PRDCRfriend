61d4d962c7e624200ead4fe722d4fbb601b01415
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2022-01-13 09:30:19 -0500

    added producer to data table

---
+++ PRDCRfriend.Data/Producer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRDCRfriend.Data
+{
+    public class Producer
+    {
+        public int ProducerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+        //public virtual List<Equipment> Equipment { get; set; } = new List<Equipment>();
+
+
+    }
+}
