b7fc8807fef2809c1d4fe8e73f6027b0dd380119
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2022-01-13 12:52:15 -0500

    Added connection string

--- PRDCRfriend.Data/Producer.cs
+++ PRDCRfriend.Data/Producer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace PRDCRfriend.Data
 {
     public class Producer
     {
+        [Key]
         public int ProducerId { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
 
 
