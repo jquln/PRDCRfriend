8f38a2a850b75f104ae0faf5c5ec2a04d1363de8
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2022-01-13 13:19:02 -0500

    Added ProducerListItem model and ProducerController

---
+++ PRDCRfriend.Models/ProducerListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRDCRfriend.Models
+{
+    public class ProducerListItem
+    {
+        public int ProducerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ PRDCRfriend.WebMVC/Controllers/ProducerController.cs
@@ -0,0 +1,20 @@
+﻿using PRDCRfriend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRDCRfriend.WebMVC.Controllers
+{
+    [Authorize]
+    public class ProducerController : Controller
+    {
+        // GET: Producer
+        public ActionResult Index()
+        {
+            var model = new ProducerListItem[0];
+            return View(model);
+        }
+    }
+}
