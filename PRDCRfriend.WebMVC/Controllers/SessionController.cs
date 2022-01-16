bd885a1ee6894ce1665beadfcc901e60703864e7
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2022-01-15 20:17:50 -0500

    Added Session Controller

---
+++ PRDCRfriend.WebMVC/Controllers/SessionController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNet.Identity;
+using PRDCRfriend.Models.SessionModels;
+using PRDCRfriend.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRDCRfriend.WebMVC.Controllers
+{
+    [Authorize]
+    public class SessionController : Controller
+    {
+        // GET: Session
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new SessionService(userId);
+            var model = service.GetSessions();
+
+            return View(model);
+        }
+
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Add code here VVVV
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SessionCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateSessionService();
+
+            if (service.CreateSession(model))
+            {
+                TempData["SaveResult"] = "Your Recording Session was scheduled!";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Your Recording Session was not scheduled ]:");
+
+            return View(model);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private SessionService CreateSessionService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new SessionService(userId);
+            return service;
+        }
+
+    }
+}
